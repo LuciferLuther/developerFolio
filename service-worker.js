fc4e1c42e3b476bd22cb569eb8072434d199d622
LuciferLuther <56398109+LuciferLuther@users.noreply.github.com>
2022-06-26 04:53:07 +0000

    Deploying to gh-pages from  @ db1b46146332cd46b63fa6ca7c5a738b4ecaef3f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5316c3a10172edf9caa8b368b30aa134.js"
+  "/precache-manifest.8292ad188cf5a6b104054d7a44f92086.js"
 );
 
 self.addEventListener('message', (event) => {
